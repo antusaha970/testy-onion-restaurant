a478807ae56508972bf0f824cb515f50069d89ff
antu <antusaha970@gmail.com>
2023-02-23 12:12:34 +0600

    Hero area coded

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavArea from './components/NavArea/NavArea';
 import './App.css';
+import HeroArea from './components/HeroArea/HeroArea';
 
 function App() {
   return (
     <div className="App">
       <NavArea></NavArea>
+      <HeroArea></HeroArea>
     </div>
   );
 }
---
+++ src/components/HeroArea/HeroArea.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './HeroArea.css';
+
+const HeroArea = () => {
+    return (
+        <header>
+            <div className="hero-container d-flex justify-content-center align-items-center">
+               <div className="titles text-center">
+                 <h1 className='fw-bold'>Best foods waiting for your belly</h1>
+                 <form className=' mt-2'>
+                    <input type="text" placeholder='Search for food' className='food-inp'/>
+                    <input type="submit" className='custom-btn' />
+                 </form>
+               </div>
+            </div>
+        </header>
+    );
+};
+
+export default HeroArea;
